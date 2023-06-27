775b0b5bb33cb7d8fb5c3c341899dbdc663532e2
Kiarash2473 <82597013+Kiarash2473@users.noreply.github.com>
2023-06-27 15:33:35 +0330

    Update Inventory.cpp
    Add update_curr_ratio function to inventory class.

--- Inventory.cpp
+++ Inventory.cpp
@@ -140,6 +140,58 @@ public:
 		else
 			cout << "No currency was found with this name." << endl;
 	}
+
+void update_curr_ratio(string from, string to, double ratio)
+	{
+		for (ar x : from)
+			x = tolower(x);
+		for (ar x : to)
+			x = tolower(x);
+
+		if (from == "usd")
+		{
+			if (to == "usd")
+				cout << "the source and destination currencies are the same." << endl;
+			else if (to == "eur")
+			{
+				this->eur_to_usd = 1 / ratio;
+				cout << "successfully updated." << endl;
+			}
+			else if (to == "gbp")
+			{
+				this->gbp_to_usd = 1 / ratio;
+				cout << "successfully updated." << endl;
+			}
+			else
+				cout << "invalid destination currency to update ratio." << endl;
+		}
+		else if (from == "eur")
+		{
+			if (to == "eur")
+				cout << "the source and destination currencies are the same." << endl;
+			else if (to == "usd")
+			{
+				this->eur_to_usd = ratio;
+				cout << "successfully updated." << endl;
+			}
+			else
+				cout << "invalid destination currency to update ratio." << endl;
+		}
+		else if (from == "gbp")
+		{
+			if (to == "gbp")
+				cout << "the source and destination currencies are the same." << endl;
+			else if (to == "usd")
+			{
+				this->gbp_to_usd = ratio;
+				cout << "successfully updated." << endl;
+			}
+			else
+				cout << "invalid destination currency to update ratio." << endl;
+		}
+		else
+			cout << "invalid source currency to update ratio." << endl;
+	}
 };
 
 int main()
